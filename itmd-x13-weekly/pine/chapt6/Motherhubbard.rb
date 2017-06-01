d5afd2cc0941f8aaf81fc8b6e52af6a79fab5aca
KellyLaas <laas@iit.edu>
2017-06-01 08:44:09 -0500

    Add files from week 1 to repository
    Due a to a incredible f**k-up on my part, managed to entirely screw up
    the repository I created during the first week of the summer. From now
    on, have better understanding of git but had to start from scratch.

--- itmd-x13-weekly/pine/chapt6/Motherhubbard.rb
+++
@@ -1,7 +0,0 @@
-line_width = 50
-puts (            'Old Mother Hubbard'.center(line_width))
-puts (           'Sat in her cupboard'.center(line_width))
-puts (          'Eating her curds and whey'.center(line_width))
-puts (         'When along came a spider'.center(line_width))
-puts (         'And sat down beside her'.center(line_width))
-puts ('And scared the poor darling away'.center(line.width))
